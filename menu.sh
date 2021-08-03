a4d681c4b1bf135099663329f0915007575b3678
Daniel Yount <factorf2@yahoo.com>
2021-08-02 20:48:59 -0500

    Create menu.sh
    Bash Menu script for setup and managing SSH Team Keys.

---
+++ menu.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+# A ssh team key menu driven script  
+## ----------------------------------
+# Define variables
+# ----------------------------------
+EDITOR=vim
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+
+BASEDIR=/home/dyount
+ 
+# ----------------------------------
+# User defined function
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+sshdsetup(){
+        echo "SSHD setup directory and config file for separate team key and need to restart service"
+
+        #Need to check if team key has already been changed by search for authorized_team_keys
+        #Make directory for original files. 
+        if [ -f "$BASEDIR/sshteamkeys/orig" ]; then
+         echo "Backup directory orig already exists"
+        else
+         mkdir $BASEDIR/sshteamkeys/orig/
+         echo "Made backup directory..."
+        fi 
+  
+        if [ -f "$BASEDIR/sshteamkeys/orig/sshd_config" ]; then
+         echo "File exists in orig backup directory"
+        else
+        cp /etc/ssh/sshd_config $BASEDIR/sshteamkeys/orig/
+         echo "Made backup of sshd config..."
+        sed -i 's/.*AuthorizedKeysFile.*/AuthorizedKeysFile .ssh\/authorized_keys .ssh\/authorized_team_keys/' /etc/ssh/sshd_config 
+        echo "Made change to sshd_connig file."
+        fi 
+        pause
+}
+ 
+sshadd(){
+        echo "sshadd() called"
+        pause
+}
+ 
+sshremove(){
+        echo "sshremove() called"
+        pause
+}
+
+sshclearall(){
+        echo "sshclearall() called"
+        pause
+}
+
+
+sshsetlogin(){
+        echo "sshsetlogin() called"
+        pause
+}
+
+sshsetgit(){
+        echo "sshsetgit() called"
+        pause
+}
+
+sshsetscp(){
+        echo "sshsetscp() called"
+        pause
+}
+
+sshsetrbash(){
+        echo "sshsetrbash() called"
+        pause
+}
+
+accesscomposer(){
+        echo "accesscomposer() called"
+        pause
+}
+
+accessnode(){
+        echo "accessnode() called"
+        pause
+}
+
+accessbasic(){
+        echo "accessbasic() called"
+        pause
+}
+
+# function to display menus
+show_menus() {
+        clear
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        echo " SSH TEAM KEYS - M E N U"
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~"
+        echo "1. SSHD SETUP"
+        echo "2. ADD KEY"
+        echo "3. REMOVE KEY"
+        echo "4. CLEAR ALL TEAM KEYS"
+
+        echo "5. SET LOGIN DIR"
+        echo "6. SET KEY FOR GIT"
+        echo "7. SET KEY FOR SCP"
+        echo "8. SET KEY FOR RBASH"
+
+        echo "9. ACCESS FOR COMPOSER"
+        echo "0. ACCESS FOR NODE"
+        echo "a. ACCESS FOR BASIC COMMANDS"
+
+        echo "q. Exit"
+}
+# read input from the keyboard and take a action
+# invoke the one() when the user select 1 from the menu option.
+# invoke the two() when the user select 2 from the menu option.
+# Exit when user the user select 3 form the menu option.
+read_options(){
+        local choice
+        read -p "Enter choice [1 2 3 4 5 6 7 8 9 0 a q] " choice
+        case $choice in
+                1) sshdsetup ;;
+                2) sshadd ;;
+                3) sshremove ;;
+                4) sshclearall ;;
+                5) sshsetlogin ;;
+                6) sshsetgit ;;
+                7) sshsetscp ;;
+                8) sshsetrbash ;;
+                9) accesscomposer ;;
+                0) accessnode ;;
+                a) accessbasic ;;
+                q) exit 0;;
+
+                *) echo -e "${RED}Error...${STD}" && sleep 2
+        esac
+}
+ 
+# ----------------------------------------------
+# Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Main logic - infinite loop
+# ------------------------------------
+while true
+do
+ 
+        show_menus
+        read_options
+done
